409e624620f3cb3338204208d6b02ef51b453826
WindHide <WindHide520@gmail.com>
2022-07-10 22:45:03 +0800

    [M]更改了Index的组件加载逻辑，动态加载

--- src/views/rootIndex.vue
+++ src/views/rootIndex.vue
@@ -8,13 +8,13 @@
 					<div class="cleftbg_p">光遇攻略站后台</div>
 				</div>
 			    <li id="cleft_li01" style="background-color: #ececec;"><img src="/api/font/root01.webp" /><span>后台</span></li>
-			    <li id="cleft_li03"><img src="/api/font/root02.webp" /><span>先祖管理</span></li>
-			    <li id="cleft_li04"><img src="/api/font/root03.webp" /><span>物品管理</span></li>
-			    <li id="cleft_li05"><img src="/api/font/root04.webp" /><span>复刻管理</span></li>
-			    <li id="cleft_li07"><img src="/api/font/root05.webp" /><span>骗子管理</span></li>
-			    <li id="cleft_li08"><img src="/api/font/root06.webp" /><span>活动管理</span></li>
-			    <li id="cleft_li09"><img src="/api/font/root07.webp" /><span>留言管理</span></li>
-			    <li id="cleft_li10"><img src="/api/font/root08.webp" /><span>季节管理</span></li>
+			    <a @click="changeState(3)"><li id="cleft_li03"><img src="/api/font/root02.webp" /><span>先祖管理</span></li></a>
+			    <a @click="changeState(4)"><li id="cleft_li04"><img src="/api/font/root03.webp" /><span>物品管理</span></li></a>
+			    <a @click="changeState(5)"><li id="cleft_li05"><img src="/api/font/root04.webp" /><span>复刻管理</span></li></a>
+			    <a @click="changeState(7)"><li id="cleft_li07"><img src="/api/font/root05.webp" /><span>骗子管理</span></li></a>
+			    <a @click="changeState(8)"><li id="cleft_li08"><img src="/api/font/root06.webp" /><span>活动管理</span></li></a>
+			    <a @click="changeState(9)"><li id="cleft_li09"><img src="/api/font/root07.webp" /><span>留言管理</span></li></a>
+			    <a @click="changeState(10)"><li id="cleft_li10"><img src="/api/font/root08.webp" /><span>季节管理</span></li></a>
 			</ul>
 			<!-- 内容左侧选择区域 -->
 			<!-- 
@@ -27,22 +27,22 @@
 					<rootNav01 />
 				</div>
 				<div id="nav03">
-					<rootNav03 />
+					<rootNav03 v-if="state3" />
 				</div>
 				<div id="nav04">
- 					<rootNav04  /> 
+ 					<rootNav04 v-if="state4" /> 
 				</div>
 				<div id="nav05">
-					<rootNav05 />
+					<rootNav05 v-if="state5" />
 				</div>
 				<div id="nav07"> 
-					<rootNav07 />
+					<rootNav07 v-if="state7" />
 				</div>
 				<div id="nav08">
-					<rootNav08 />
+					<rootNav08 v-if="state8" />
 				</div>
 				<div id="nav09">
-					<rootNav09 />
+					<rootNav09 v-if="state9" />
 				</div>
 				<!-- <div id="nav10"> -->
 					<!-- <Root_Nav10 /> -->
@@ -57,28 +57,32 @@
 </template>
 
 
-<script>
-import rootNav01 from "../components/Root/rootNav01.vue";
-import rootNav03 from "../components/Root/rootNav03.vue";
-import rootNav04 from "../components/Root/rootNav04.vue";
-import rootNav05 from "../components/Root/rootNav05.vue";
-import rootNav07 from "../components/Root/rootNav07.vue";
-import rootNav08 from "../components/Root/rootNav08.vue";
-import rootNav09 from "../components/Root/rootNav09.vue";
-import rootNav10 from "../components/Root/rootNav10.vue";
-export default{
-	name: "Index",
-    components: {
-	  	rootNav01,
-		rootNav03,
-		rootNav04,
-		rootNav05,
-		rootNav07,
-		rootNav08,
-		rootNav09,
-		rootNav10
-    },
-}
+<script lang="ts" setup>
+	import rootNav01 from "../components/Root/rootNav01.vue";
+	import rootNav03 from "../components/Root/rootNav03.vue";
+	import rootNav04 from "../components/Root/rootNav04.vue";
+	import rootNav05 from "../components/Root/rootNav05.vue";
+	import rootNav07 from "../components/Root/rootNav07.vue";
+	import rootNav08 from "../components/Root/rootNav08.vue";
+	import rootNav09 from "../components/Root/rootNav09.vue";
+	import rootNav10 from "../components/Root/rootNav10.vue";
+
+	import { reactive,nextTick,ref } from 'vue-demi';
+	
+	let state3:any = ref(false);
+	let state4:any = ref(false);
+	let state5:any = ref(false);
+	let state7:any = ref(false);
+	let state8:any = ref(false);
+	let state9:any = ref(false);
+	let state10:any = ref(false);
+
+	function changeState(item:Number){
+		eval("state"+item+".value=false")
+		nextTick(function () {
+            eval("state"+item+".value=true") // 重新加载组件.
+        }) 
+	}
 </script>
 
 <style>
--- src/views/userIndex.vue
+++ src/views/userIndex.vue
@@ -14,13 +14,13 @@
 				</div>
 			    <li id="cleft_li01" style="background-color: #ececec;"><img src="/api/font/root01.webp" /><span>网站首页</span></li>
 			    <li id="cleft_li02"><img src="/api/font/root02.webp" /><span>关于我</span></li>
-			    <a @click="clickADD(3)"><li id="cleft_li03"><img src="/api/font/user04.webp" /><span>先祖信息</span></li></a>
-			    <a @click="clickADD(4)"><li id="cleft_li04"><img src="/api/font/user05.webp" /><span>物品信息</span></li></a>
-			    <a @click="clickADD(5)"><li id="cleft_li05"><img src="/api/font/user02.webp" /><span>复刻信息</span></li></a>
-			    <a @click="clickADD(7)"><li id="cleft_li07"><img src="/api/font/user07.webp" /><span>骗子预警</span></li></a>
-			  	<a @click="clickADD(8)"><li id="cleft_li08"><img src="/api/font/user08.webp" /><span>活动时间</span></li></a>
-				<a @click="clickADD(9)"><li id="cleft_li09"><img src="/api/font/user03.webp" /><span>奇妙留言</span></li></a>
-				<a @click="clickADD(10)"><li id="cleft_li10"><img src="/api/font/user01.webp" /><span>季节信息</span></li></a>
+			    <a @click="changeState(3)"><li id="cleft_li03"><img src="/api/font/user04.webp" /><span>先祖信息</span></li></a>
+			    <a @click="changeState(4)"><li id="cleft_li04"><img src="/api/font/user05.webp" /><span>物品信息</span></li></a>
+			    <a @click="changeState(5)"><li id="cleft_li05"><img src="/api/font/user02.webp" /><span>复刻信息</span></li></a>
+			    <a @click="changeState(7)"><li id="cleft_li07"><img src="/api/font/user07.webp" /><span>骗子预警</span></li></a>
+			  	<a @click="changeState(8)"><li id="cleft_li08"><img src="/api/font/user08.webp" /><span>活动时间</span></li></a>
+				<a @click="changeState(9)"><li id="cleft_li09"><img src="/api/font/user03.webp" /><span>奇妙留言</span></li></a>
+				<a @click="changeState(10)"><li id="cleft_li10"><img src="/api/font/user01.webp" /><span>季节信息</span></li></a>
 				<div style="margin-top:5vh">
 					<el-button type="danger" plain round v-if="getloginUsername()!=''" @click="logOut()" >退出登录 😫</el-button>
 					<el-button type="primary" plain round v-else  @click="login()">太炫酷了马上去登录 🥳</el-button>
@@ -46,31 +46,31 @@
 				</div>
 				<!-- 第三页 - 先祖信息 -->
 				<div id="nav03">
-					<userNav03 v-if="StateCheck(3)" />
+					<userNav03 v-if="state3"  />
 				</div>
 				<!-- 第四页 - 物品信息 -->
 				<div id="nav04">
-					<userNav04 v-if="StateCheck(4)" />
+					<userNav04 v-if="state4"  />
 				</div>
 				<!-- 第五页 - 复刻信息 -->
 				<div id="nav05">
-					<userNav05 v-if="StateCheck(5)" />
+					<userNav05 v-if="state5"  />
 				</div>
 				<!-- 第七页 - 骗子预警 -->
 				<div id="nav07">
-					<userNav07 v-if="StateCheck(7)" />
+					<userNav07 v-if="state7"  />
 				</div> 
 				<!-- 第八页 - 活动 -->
 				<div id="nav08">
-					<userNav08  v-if="StateCheck(8)"/>
+					<userNav08  v-if="state8" />
 				</div>
 				<!-- 第九页 - 留言 -->
 				<div id="nav09">
-					<userNav09  v-if="StateCheck(9)"/>
+					<userNav09  v-if="state9" />
 				</div>
 				<!-- 第十页 - 后台管理 -->
 				<div id="nav10">
-					<userNav10  v-if="StateCheck(10)"/>
+					<userNav10  v-if="state10" />
 				</div>
 			</div>
 		</div>
@@ -81,70 +81,60 @@
 	</div>
 </template>
 
-<script>
-import userNav01 from '@/components/User/userNav01'
-import userNav02 from '@/components/User/userNav02'
-import userNav03 from '@/components/User/userNav03'
-import userNav04 from '@/components/User/userNav04'
-import userNav05 from '@/components/User/userNav05'
-import userNav07 from '@/components/User/userNav07'
-import userNav08 from '@/components/User/userNav08'
-import userNav09 from '@/components/User/userNav09'
-import userNav10 from '@/components/User/userNav10'
-import {mapState} from "vuex";
-export default {
-  name: "Index",
-  components: {
-    userNav01,
-    userNav02,
-    userNav03,
-    userNav04,
-    userNav05,
-    userNav07,
-    userNav08,
-    userNav09,
-    userNav10,
-  },
-  data(){
-	  return{
-		  ifShow:1,
-		  ShowState:[],
-	  }
-  },
-  methods:{
-	  goRoot(){
+<script lang="ts" setup>
+    import { notify_messeage } from '@/apis/defineFunction'
+	import router from '@/router/routerIndex'
+	import userNav01 from "@/components/User/userNav01.vue";
+	import userNav02 from "@/components/User/userNav02.vue";
+	import userNav03 from "@/components/User/userNav03.vue";
+	import userNav04 from "@/components/User/userNav04.vue";
+	import userNav05 from "@/components/User/userNav05.vue";
+	import userNav07 from "@/components/User/userNav07.vue";
+	import userNav08 from "@/components/User/userNav08.vue";
+	import userNav09 from "@/components/User/userNav09.vue";
+	import userNav10 from "@/components/User/userNav10.vue";
+	import { reactive,nextTick,ref } from 'vue-demi';
+	
+	let state3:any = ref(false);
+	let state4:any = ref(false);
+	let state5:any = ref(false);
+	let state7:any = ref(false);
+	let state8:any = ref(false);
+	let state9:any = ref(false);
+	let state10:any = ref(false);
+
+	function goRoot(){
 		setTimeout(async () => {
-				await this.$router.push('/root');
-		  		await this.$router.go(0) 
-						// 方法区
+			await router.push('/root');
+		  	await router.go(0) 
+					// 方法区
 		}, 500);
-	  },
-	  login(){
-		  this.$router.push('/login');
-	  },
-	  logOut(){
-		  localStorage.setItem('Authorization','')
-		  localStorage.setItem('loginUsername','')
-		  localStorage.setItem('loginID','')
-		  this.notify_messeage("注销用户成功,请等待网页刷新","success")
-		  setTimeout(async() => {
-		  	this.$router.go(0) 
-		  }, 500);
-	  },
-	  getloginUsername(){
-		  return localStorage.getItem('loginUsername')
-	  },
-	  clickADD(item){
-		  this.ShowState.push(item);
-	  },
-	  StateCheck(item){
-		  for(let i = 0 ; i < this.ShowState.length ; i++){
-			  if(this.ShowState[i] == item)	return true;
-		  }
-		  return false;
-	  }
-  },
-};
+	}
+	
+	function login(){
+		router.push('/login');
+	}
+
+	function logOut(){
+	  localStorage.setItem('Authorization','')
+	  localStorage.setItem('loginUsername','')
+	  localStorage.setItem('loginID','')
+	  notify_messeage("注销用户成功,请等待网页刷新","success")
+	  	setTimeout(async() => {
+	  		router.go(0) 
+	  	}, 500);
+	}
+
+	function getloginUsername(){
+		return localStorage.getItem('loginUsername')
+	}
+
+	function changeState(item:Number){
+		eval("state"+item+".value=false")
+		nextTick(function () {
+            eval("state"+item+".value=true") // 重新加载组件.
+        }) 
+	}
 </script>
 
 <style lang="scss">
